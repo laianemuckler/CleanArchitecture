2d719a3cc636b16f6a2fe2b8c370cd61068b35ab
Laiane Muckler <laianecm@gmail.com>
2022-08-08 19:48:44 -0300

    first commit

---
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        // método de extensão
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            // registro do contexto
+            // string de conexão com o banco
+            // indicação de onde será criada a pasta migrations
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            // recomendação para aplicações web é utilizar o AddScoped
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+
+        }
+    }
+}
